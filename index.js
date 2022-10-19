f0f5330bfa1c37997dfdd0710c6bdd05ca8e4ad4
Gary Almes <garytalmes@gmail.com>
2022-10-19 15:05:54 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,84 @@
+const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+require("console.table");
+
+// This file leads to a class we've created to contain all our database queries
+const db = require("./db");
+
+
+// Use this function to display the ascii art logo and to begin the main prompts
+function init() {
+
+
+  loadMainPrompts()
+}
+
+
+
+// Here we load the initial prompts with a series of options. The first option is provided for you.
+function loadMainPrompts() {
+  prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        {
+          name: "View All Employees",
+          value: "VIEW_EMPLOYEES"
+        },
+
+        // add more options here
+      ]
+    }
+  ]).then(res => {
+    let choice = res.choice;
+    // Call the appropriate function depending on what the user chose
+    
+    switch (choice) {
+      case "VIEW_EMPLOYEES":
+        viewEmployees();
+        break;
+      
+        // add the other case statements here
+    }
+  }
+)}
+
+
+
+
+/* ======= Controllers ============================================================ */
+
+// Here is a function which handles the first prompt option:  View all employees
+function viewEmployees() {
+
+  // Here we call the method in the db file for finding all employees.
+  // we get the result back, and then display the result 
+  db.findAllEmployees()
+    .then(([rows]) => {
+      let employees = rows;
+      console.log("\n");
+      console.table(employees);
+    })
+    .then(() => loadMainPrompts());
+}
+
+
+
+/* ======= END Controllers ============================================================ */
+
+
+
+
+
+/* 
+  You will write lots of other functions here for the other prompt options.
+  Note that some prompts will require you to provide more prompts, and these 
+  may need functions of their own.
+*/
+
+
+
+// Everything starts here!
+init();
